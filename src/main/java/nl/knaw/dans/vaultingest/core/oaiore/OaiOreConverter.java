e660d99c832cfb207655495ff2f955dda2872466
Jan van Mansum <janvanmansum@gmail.com>
2026-01-09 22:22:30 +0100

    Restored correct formatting of ResourceMap URI

--- src/main/java/nl/knaw/dans/vaultingest/core/oaiore/OaiOreConverter.java
+++ src/main/java/nl/knaw/dans/vaultingest/core/oaiore/OaiOreConverter.java
@@ -131,7 +131,7 @@ public class OaiOreConverter {
     }
 
     Resource createResourceMap(Deposit deposit, Model model) {
-        var resourceMap = model.createResource("urn:uuid" + deposit.getId());
+        var resourceMap = model.createResource("urn:uuid:" + deposit.getId());
         var resourceMapType = model.createStatement(resourceMap, RDF.type, ORE.ResourceMap);
 
         model.add(resourceMapType);
